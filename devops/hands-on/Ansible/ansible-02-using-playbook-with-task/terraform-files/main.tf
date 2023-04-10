83d0ce727ade9134b5b2f124bfa64a235306b46e
oliver <oliver@clarusway.com>
2023-04-10 18:28:59 +0300

    ansible handson added2

--- devops/hands-on/Ansible/ansible-02-using-playbook-with-task/terraform-files/main.tf
+++ devops/hands-on/Ansible/ansible-02-using-playbook-with-task/terraform-files/main.tf
@@ -89,13 +89,15 @@ resource "null_resource" "config" {
   provisioner "remote-exec" {
     inline = [
       "sudo hostnamectl set-hostname Control-Node",
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install ansible2 -y",
+      "sudo dnf update -y",
+      "curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py",
+      "python3 get-pip.py --user",
+      "pip3 install --user ansible",
       "echo [webservers] >> inventory.txt",
-      "echo node1 ansible_host=${aws_instance.nodes[1].private_ip} ansible_ssh_private_key_file=~/tyler-team.pem ansible_user=ec2-user >> inventory.txt",
-      "echo node2 ansible_host=${aws_instance.nodes[2].private_ip} ansible_ssh_private_key_file=~/tyler-team.pem ansible_user=ec2-user >> inventory.txt",
+      "echo node1 ansible_host=${aws_instance.nodes[1].private_ip} ansible_ssh_private_key_file=~/${var.mykey}.pem ansible_user=ec2-user >> inventory.txt",
+      "echo node2 ansible_host=${aws_instance.nodes[2].private_ip} ansible_ssh_private_key_file=~/${var.mykey}.pem ansible_user=ec2-user >> inventory.txt",
       "echo [ubuntuservers] >> inventory.txt",
-      "echo node3 ansible_host=${aws_instance.nodes[3].private_ip} ansible_ssh_private_key_file=~/tyler-team.pem ansible_user=ubuntu >> inventory.txt",
+      "echo node3 ansible_host=${aws_instance.nodes[3].private_ip} ansible_ssh_private_key_file=~/${var.mykey}.pem ansible_user=ubuntu >> inventory.txt",
       "chmod 400 ${var.mykey}.pem"
     ]
   }
--- devops/hands-on/Ansible/ansible-02-using-playbook-with-task/terraform-files/myvars.auto.tfvars
+++ devops/hands-on/Ansible/ansible-02-using-playbook-with-task/terraform-files/myvars.auto.tfvars
@@ -1,6 +1,6 @@
 //aws_access_key = ""
 //aws_secret_key = ""
-myami = ["ami-0022f774911c1d690", "ami-0022f774911c1d690", "ami-0022f774911c1d690", "ami-0c4f7023847b90238"]
+myami = ["ami-06e46074ae430fba6", "ami-06e46074ae430fba6", "ami-06e46074ae430fba6", "ami-0aa2b7722dc1b5612"]
 mykey = "clarusway"
 tags = ["control_node", "node_1", "node_2", "node3"]
 instancetype = "t2.micro"
