53ca69f446f25dc1e58811f01a51f87f9094b711
oliver <oliver@clarusway.com>
2023-05-15 21:59:08 +0300

    jenkins project modified4

--- devops/hands-on/ECR/ecr-02a-jenkins-pipeline-to-deploy-docker-images-on-ECR-to-ECS-cluster/ecr-ecs-ec2-jenkins.tf
+++ devops/hands-on/ECR/ecr-02a-jenkins-pipeline-to-deploy-docker-images-on-ECR-to-ECS-cluster/ecr-ecs-ec2-jenkins.tf
@@ -1,6 +1,6 @@
 /*This terraform file creates a Jenkins Server using JDK 11 on EC2 Instance.
   Jenkins Server is enabled with Git, Docker and Docker Compose,
-  AWS CLI Version 2. Jenkins Server will run on Amazon Linux 2 EC2 Instance with
+  AWS CLI Version 2. Jenkins Server will run on Amazon Linux 2023 EC2 Instance with
   custom security group allowing HTTP(80, 8080) and SSH (22) connections from anywhere. 
 */
 
@@ -18,11 +18,13 @@ provider "github" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  github-email    = "serdar@clarusway.com"   # you need to change this line
-  github-username = "serdarcw"               # you need to change this line
-  github-token    = ""                       # you need to change this line
-  key_pair        = ""                       # you need to change this line
-  pem_key_address = "~/Downloads/serdar.pem" # you need to change this line
+  github-email    = "oliver@clarusway.com"     # you need to change this line
+  github-username = "clarusway"                # you need to change this line
+  github-token    = "xxxxxxxxx"                # you need to change this line
+  key_pair        = "clarus"                   # you need to change this line
+  pem_key_address = "~/.ssh/clarus.pem"        # you need to change this line
+  instance_type   = "t3a.small"
+  user            = "clarusway"
 }
 
 resource "github_repository" "githubrepo" {
@@ -30,13 +32,12 @@ resource "github_repository" "githubrepo" {
   visibility = "private"
 }
 
-
 variable "sg-ports" {
   default = [80, 22, 8080]
 }
 
 resource "aws_security_group" "ec2-sec-gr" {
-  name = "jenkins-sec-gr"
+  name = "jenkins-sec-gr-${local.user}"
   tags = {
     Name = "jenkins-sec-gr"
   }
@@ -57,9 +58,8 @@ resource "aws_security_group" "ec2-sec-gr" {
   }
 }
 
-
 resource "aws_iam_role" "roleforjenkins" {
-  name                = "ecr_jenkins_permission"
+  name                = "ecr_jenkins_permission_${local.user}"
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess", "arn:aws:iam::aws:policy/AdministratorAccess", "arn:aws:iam::aws:policy/AmazonECS_FullAccess"]
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -77,58 +77,66 @@ resource "aws_iam_role" "roleforjenkins" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "jenkinsprofile"
+  name = "jenkinsprofile-${local.user}"
   role = aws_iam_role.roleforjenkins.name
 }
 
+data "aws_ami" "al2023" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
 resource "aws_instance" "jenkins-server" {
-  ami           = "ami-087c17d1fe0178315"
-  instance_type = "t2.small"
+  ami           = data.aws_ami.al2023.id
+  instance_type = local.instance_type
   key_name      = local.key_pair
   root_block_device {
     volume_size = 16
   }
-  security_groups = ["jenkins-sec-gr"]
+  vpc_security_group_ids = [aws_security_group.ec2-sec-gr.id]
   tags = {
-    Name = "Jenkins-Server"
+    Name = "Jenkins-Server-${local.user}"
   }
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   user_data            = <<-EOF
           #! /bin/bash
-          # install git
-          yum install git -y
           # update os
-          yum update -y
+          dnf update -y
+          # install git
+          dnf install git -y
           # set server hostname as Jenkins-Server
           hostnamectl set-hostname "Jenkins-Server"
           # install java 11
-          amazon-linux-extras install java-openjdk11 -y
-          wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-          rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-          amazon-linux-extras install epel -y
+          dnf upgrade
+          dnf install java-11-amazon-corretto -y
+          wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+          rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
           # install jenkins
-          yum install jenkins -y
-          systemctl start jenkins
+          dnf install jenkins -y
           systemctl enable jenkins
+          systemctl start jenkins
           # install docker
-          amazon-linux-extras install docker -y
+          dnf install docker -y
           systemctl start docker
           systemctl enable docker
-          # add ec2-user and jenkins users to docker group 
+          #add ec2-user and jenkins users to docker group
           usermod -a -G docker ec2-user
           usermod -a -G docker jenkins
           # configure docker as cloud agent for jenkins
           cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
-          sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
-          # systemctl daemon-reload
+          sed -i 's/^ExecStart=*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
+          systemctl daemon-reload
           systemctl restart docker
           systemctl restart jenkins
-          # uninstall aws cli version 1
-          rm -rf /bin/aws
-          # install aws cli version 2
-          curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-          unzip awscliv2.zip
-          ./aws/install
           EOF
 
   provisioner "remote-exec" {
@@ -155,8 +163,6 @@ resource "aws_instance" "jenkins-server" {
   }
 }
 
-
-
 output "jenkins-dns-url" {
   value = "http://${aws_instance.jenkins-server.public_ip}:8080"
 }
@@ -176,4 +182,3 @@ output "github-url" {
 output "aws-account-id" {
   value = data.aws_caller_identity.current.account_id
 }
-
