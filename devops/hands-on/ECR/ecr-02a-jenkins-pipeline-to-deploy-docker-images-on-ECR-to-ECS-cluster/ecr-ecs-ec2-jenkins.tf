aad193cdfab44e015e173d16ab3b7e96f5fa4ea7
oliver <oliver@clarusway.com>
2023-05-17 12:41:14 +0300

    jenkins project modified7

--- devops/hands-on/ECR/ecr-02a-jenkins-pipeline-to-deploy-docker-images-on-ECR-to-ECS-cluster/ecr-ecs-ec2-jenkins.tf
+++ devops/hands-on/ECR/ecr-02a-jenkins-pipeline-to-deploy-docker-images-on-ECR-to-ECS-cluster/ecr-ecs-ec2-jenkins.tf
@@ -133,7 +133,7 @@ resource "aws_instance" "jenkins-server" {
           usermod -a -G docker jenkins
           # configure docker as cloud agent for jenkins
           cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
-          sed -i 's/^ExecStart=*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
+          sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2376 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
           systemctl daemon-reload
           systemctl restart docker
           systemctl restart jenkins
@@ -141,7 +141,7 @@ resource "aws_instance" "jenkins-server" {
 
   provisioner "remote-exec" {
     inline = [
-      "sleep 180",
+      "sleep 210",
       "wget https://github.com/awsdevopsteam/jenkins-first-project/raw/master/to-do-app-nodejs.tar",
       "tar -xvf to-do-app-nodejs.tar",
       "rm to-do-app-nodejs.tar",
--- devops/projects/208-jenkins-pipeline-for-webpage-application(postgresql-nodejs-react)/jenkins_server/jenkins.sh
+++ devops/projects/208-jenkins-pipeline-for-webpage-application(postgresql-nodejs-react)/jenkins_server/jenkins.sh
@@ -15,7 +15,7 @@ systemctl enable docker
 usermod -a -G docker ec2-user
 usermod -a -G docker jenkins
 cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
-sed -i 's/^ExecStart=*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
+sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2376 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
 systemctl daemon-reload
 systemctl restart jenkins
 curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
