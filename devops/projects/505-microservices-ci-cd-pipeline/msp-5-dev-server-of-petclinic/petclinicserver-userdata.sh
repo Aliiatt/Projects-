951a265f76299593178a170e4e1f5414f4b8295c
oliver <oliver@clarusway.com>
2023-05-23 22:26:23 +0300

    project505 modified

--- devops/projects/505-microservices-ci-cd-pipeline/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
+++ devops/projects/505-microservices-ci-cd-pipeline/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-yum update -y
+dnf update -y
 hostnamectl set-hostname petclinic-dev-server
-amazon-linux-extras install docker -y
+dnf install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
--o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-yum install git -y
-yum install java-11-amazon-corretto -y
-git clone https://github.com/clarusway/petclinic-microservices-with-db.git
+dnf install git -y
+dnf install java-11-amazon-corretto -y
+cd /home/ec2-user && git clone https://github.com/clarusway/petclinic-microservices-with-db.git
 git checkout dev
