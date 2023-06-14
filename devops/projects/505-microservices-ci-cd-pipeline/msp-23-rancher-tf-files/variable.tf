f0f3b48b280635271bc587b2d98b8aac75b2d010
oliver <oliver@clarusway.com>
2023-06-14 20:04:20 +0300

    project505 handsons modified7

--- devops/projects/505-microservices-ci-cd-pipeline/msp-23-rancher-tf-files/variable.tf
+++ devops/projects/505-microservices-ci-cd-pipeline/msp-23-rancher-tf-files/variable.tf
@@ -11,7 +11,7 @@ variable "tags" {
 }
 variable "myami" {
   description = "ubuntu 20.04 ami"
-  default = "ami-0778521d914d23bc1"
+  default = "ami-0261755bbcb8c4a84"
 }
 variable "instancetype" {
   default = "t3a.medium"
@@ -31,4 +31,32 @@ variable "rancher-subnet" {
 
 variable "hostedzone" {
   default = "clarusway.us"
+}
+
+variable "tg-name" {
+  default = "clarus-rancher-http-80-tg-oliver"
+}
+
+variable "alb-name" {
+  default = "petclinic-rancher-alb-oliver"
+}
+
+variable "controlplane-policy-name" {
+  default = "petclinic_policy_for_rke-controlplane_role"
+}
+
+variable "worker-policy-name" {
+  default = "petclinic_policy_for_rke_etcd_worker_role"
+}
+
+variable "rancher-role" {
+  default = "petclinic_role_rancher-oliver"
+}
+
+variable "controlplane-attach" {
+  default = "petclinic_attachment_for_rancher_controlplane"
+}
+
+variable "worker-attach" {
+  default = "petclinic_attachment_for_rancher_controlplane"
 }
